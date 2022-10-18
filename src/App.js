0ec9fb06b58a7de95edc36ab8a9b1cf1aa08110a
widiagustin <widhianiagustin14@gmail.com>
2022-10-18 11:15:08 +0700

    feat: add routing for pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Dashboard />} />
+        <Route path='about' element={<About />} />
+        <Route path='projects' element={<Projects />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
